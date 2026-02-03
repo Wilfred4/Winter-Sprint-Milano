95d756726e2cda38b91b7e4c036d70456b0782a6
YazidBbz <y.bouabizi@gmail.com>
2026-02-03 10:51:01 +0100

    chore: consolidate code change entries for better organization

--- game/core/game.py
+++ game/core/game.py
@@ -8,7 +8,8 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Runner 2D")
-        self.screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
+        self.fullscreen = settings.FULLSCREEN
+        self.screen = self._set_display()
         self.clock = pygame.time.Clock()
         self.scene = MenuScene(self)
         self.running = True
@@ -16,14 +17,21 @@ class Game:
     def change_scene(self, scene):
         self.scene = scene
 
+    def _set_display(self):
+        flags = pygame.SCALED
+        if self.fullscreen:
+            flags |= pygame.FULLSCREEN
+        return pygame.display.set_mode((settings.WIDTH, settings.HEIGHT), flags)
+
     def run(self):
         while self.running:
             dt = self.clock.tick(settings.FPS)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    self.running = False
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
+                    self.fullscreen = not self.fullscreen
+                    self.screen = self._set_display()
                 self.scene.handle_event(event)
 
             self.scene.update(dt)
